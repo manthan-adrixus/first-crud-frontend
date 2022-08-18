00181b7be7771be7ac7c73ee0134573cb133368b
Manthan Machhi <manthan.machhi@adrixus.com>
2022-08-18 17:50:15 +0530

    updated endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  SOCKET_ENDPOINT: 'http://localhost:3000/',
+  // SOCKET_ENDPOINT: 'http://localhost:3000/',
+  SOCKET_ENDPOINT: "http://3.7.73.187:3000/",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  SOCKET_ENDPOINT: 'http://localhost:3000/',
+  // SOCKET_ENDPOINT: "http://localhost:3000/",
+  SOCKET_ENDPOINT: "http://3.7.73.187:3000/",
 };
 
 /*
